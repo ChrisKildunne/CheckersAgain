8b8003cf8cc483bd3c3ff979a20f1115e78d60ad
Chris Kildunne <136514462+ChrisKildunne@users.noreply.github.com>
2023-07-09 20:18:42 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,43 @@
 /*----- app's state (variables) -----*/
 let currentPlayer;
 let board;
-let validSquares;
-let clickedSquare = null;
-let nextSquare = null;
-let selectedPiece;
-let opposingPlayerPiece;
-let gameOver= false;
-let emptySquares;
-let jumpOverAvailable;
+const squares = document.querySelectorAll('.square');
+board = [
+  "", "red", "", "red", "", "red", "", "red",
+  "red", "", "red", "", "red", "", "red", "",
+  "", "red", "", "red", "", "red", "", "red",
+  "", "", "", "", "", "", "", "",
+  "", "", "", "", "", "", "", "",
+  "black", "", "black", "", "black", "", "black", "",
+  "", "black", "", "black", "", "black", "", "black",
+  "black", "", "black", "", "black", "", "black", ""
+];
+
+
 
 /*----- cached element references -----*/
-const squares = document.querySelectorAll('.square');
 const boardEl = document.getElementById('gameboard');
-const messageEl = document.querySelector('h2');
-const playAgain = document.querySelector('button');
-const dubJumpMessage = document.querySelector('h3')
-
-
-// Event listeners
-playAgain.addEventListener('click', init);
 
 /*----- functions -----*/
-init()
+init();
+
 function init() {
-  board = [
-    "illegal", "red", "illegal", "red", "illegal", "red", "illegal", "red",
-    "red", "illegal", "red", "illegal", "red", "illegal", "red", "illegal",
-    "illegal", "red", "illegal", "red", "illegal", "red", "illegal", "red",
-    "", "illegal", "", "illegal", "", "illegal", "", "illegal",
-    "illegal", "", "illegal", "", "illegal", "", "illegal", "",
-    "black", "illegal", "black", "illegal", "black", "illegal", "black", "illegal",
-    "illegal", "black", "illegal", "black", "illegal", "black", "illegal", "black",
-    "black", "illegal", "black", "illegal", "black", "illegal", "black", "illegal"
-  ];
-  currentPlayer = 'black';
-  gameOver = false;
+  currentPlayer = 'Black';
   render();
+  //alert('black always starts');
 }
 
 function render() {
   renderBoard();
-  validMove();
-  renderControls();
-}
-function renderControls(){
-  playAgain.style.visibility = gameOver ? 'visible' : 'hidden';
+  renderMove();
 }
 
+
+
 function renderBoard() {
-  messageEl.innerHTML = `${currentPlayer.toUpperCase()} STARTS`;
-  messageEl.style.color = 'black'
+ 
   squares.forEach((square, idx) => {
-    let pieceColor = board[idx];
-    square.innerHTML = '';
+   let pieceColor = board[idx];
     if (pieceColor === 'red') {
       const redPiece = document.createElement('div');
       redPiece.classList.add('red-piece');
@@ -63,206 +47,131 @@ function renderBoard() {
       blackPiece.classList.add('black-piece');
       square.appendChild(blackPiece);
     }
+    
   });
 }
 
-/*validMove enables pieces to move one space diagnally*/
+
+let clickedSquare=null;
+let nextSquare = null;
+
+function renderMove(){
+ validMove();
+ renderMove();
+}
+
+
 function validMove() {
-  if(!gameOver){
-  squares.forEach((square) => {
-    square.addEventListener('click', function (evt) {
-      clickedSquare = evt.target;
-      validSquares=[]
-      console.log(clickedSquare)
-      if (nextSquare && board[nextSquare.id]===currentPlayer) {
-        selectedPiece = parseInt(nextSquare.id);
-        if(currentPlayer==='black'){
+  squares.forEach(square => {
+    square.addEventListener('click', function(evt) {
+      const clickedSquare = evt.target;
+      //console.log(clickedSquare)
+      if (nextSquare) {
+        console.log(nextSquare)
+        const selectedPiece = parseInt(nextSquare.id);
+        const validSquares = [];
         if (board[selectedPiece - 7] === '') {
           validSquares.push(selectedPiece - 7);
         }
         if (board[selectedPiece - 9] === '') {
           validSquares.push(selectedPiece - 9);
         }
-      } else if(currentPlayer==='red'){
         if (board[selectedPiece + 7] === '') {
           validSquares.push(selectedPiece + 7);
         }
         if (board[selectedPiece + 9] === '') {
           validSquares.push(selectedPiece + 9);
         }
-      }
-       if (validSquares.includes(parseInt(square.id))) {
-          clickedSquare.innerHTML = nextSquare.innerHTML;//sets html of next square to the same as the last square
-          nextSquare.innerHTML = '';//clears last square-piece from html
-          nextSquare = null;
-          board[parseInt(square.id)] =board[selectedPiece];//updates board array
-          board[selectedPiece] = ''//updates board array
-          squareID = parseInt(square.id)
-          jumpOver()
-          changePlayer();
-          getWinner();
-          redKingMe(squareID,clickedSquare,nextSquare)
-       }
-      } else {
-        nextSquare = clickedSquare;
-        }      
-    });
-  });
-}
-}
-
-
-
-/*jumpOver enables piece to jump over opponents pieces only if the move is legal
-delete the opponents piece from the board*/
-function jumpOver() {
-  let jumpOverAvailable = false;
-
-  squares.forEach((square) => {
-    square.addEventListener('click', function (evt) {
-      clickedSquare = evt.target;
-      validSquares = [];
-
-      if (nextSquare && board[nextSquare.id] === currentPlayer) {
-        selectedPiece = parseInt(nextSquare.id);
-        opposingPlayerPiece = (selectedPiece + parseInt(clickedSquare.id)) / 2;
-
-        if (
-          (currentPlayer === 'black' && board[opposingPlayerPiece] === 'red') ||
-          (currentPlayer === 'red' && board[opposingPlayerPiece] === 'black')
-        ) {
-          if (currentPlayer === 'black') {
-            if (board[selectedPiece - 14] === '' && (selectedPiece - 14 > 0)) {
-              validSquares.push(selectedPiece - 14);
-            }
-            if (board[selectedPiece - 18] === '' && (selectedPiece - 18 > 0)) {
-              validSquares.push(selectedPiece - 18);
-            }
-          } else if (currentPlayer === 'red') {
-            if (board[selectedPiece + 14] === '' && (selectedPiece + 14 < 64)) {
-              validSquares.push(selectedPiece + 14);
-            }
-            if (board[selectedPiece + 18] === '' && (selectedPiece + 18 < 64)) {
-              validSquares.push(selectedPiece + 18);
-            }
-          }
-        }
-
-        if (validSquares.includes(parseInt(square.id))) {
+        if (validSquares.includes(parseInt(clickedSquare.id))) {
           clickedSquare.innerHTML = nextSquare.innerHTML;
           nextSquare.innerHTML = '';
           nextSquare = null;
-          board[parseInt(square.id)] = board[selectedPiece];
-          board[selectedPiece] = '';
-          board[opposingPlayerPiece] = '';//Clears from board array
-          squares[opposingPlayerPiece].innerHTML = '';//Clears from HTML
-          let updatedPiece = parseInt(square.id);
-          dubJump(updatedPiece);
-          getWinner();
-          redKingMe(updatedPiece);
-        }
+          //changePlayer();
+        } 
       } else {
         nextSquare = clickedSquare;
       }
+      getWinner(); 
     });
   });
 }
-function dubJump(updatedPiece) {
-  
-  jumpOverAvailable = false;
-  emptySquares = [];
-  const frontPiece9 = currentPlayer === 'black' ? board[updatedPiece - 9] : board[updatedPiece + 9];
-  const frontPiece7 = currentPlayer === 'black' ? board[updatedPiece - 7] : board[updatedPiece + 7];
-  const opposingPiece = currentPlayer === 'black' ? 'red' : 'black';
 
-  if (currentPlayer === 'black') {
-    if (updatedPiece - 14 > 0 && board[updatedPiece - 14] === ''&& frontPiece7 === opposingPiece) {
-      emptySquares.push(updatedPiece - 14);
-    }
-    if (updatedPiece - 18 > 0 && board[updatedPiece - 18] === ''&&frontPiece9 === opposingPiece) {
-      emptySquares.push(updatedPiece - 18);
-    }
-  } else if (currentPlayer === 'red') {
-    if (updatedPiece + 14 < 64 && board[updatedPiece + 14] === ''&&frontPiece7 === opposingPiece) {
-      emptySquares.push(updatedPiece + 14);
-    }
-    if (updatedPiece + 18 < 64 && board[updatedPiece + 18] === ''&&frontPiece9 === opposingPiece) {
-      emptySquares.push(updatedPiece + 18);
-    }
-  }
-  
-  if (emptySquares.length > 0) {
-    jumpOverAvailable = true;
-    dubJumpMessage.innerHTML = 'You have a another jump available go ahead and take it!'
-  }
+function playerJump(){
+  squares.forEach(square => {
+    square.addEventListener('click', function(evt) {
+      const clickedSquare = evt.target;
+      //console.log(clickedSquare)
+      if (nextSquare) {
+        console.log(nextSquare)
+        const selectedPiece = parseInt(nextSquare.id);
+        const validSquares = [];
 
-  if (!jumpOverAvailable) {
-    changePlayer();
-    dubJumpMessage.innerHTML = ''
+  if (board[selectedPiece - 14] === '') {
+    validSquares.push(selectedPiece - 14);
   }
+  if (board[selectedPiece - 18] === '') {
+    validSquares.push(selectedPiece - 18);
+  }
+  if (board[selectedPiece + 14] === '') {
+    validSquares.push(selectedPiece + 14);
+  }
+  if (board[selectedPiece + 18] === '') {
+    validSquares.push(selectedPiece + 18);
+  }
+  if (validSquares.includes(parseInt(clickedSquare.id))) {
+    clickedSquare.innerHTML = nextSquare.innerHTML;
+    nextSquare.innerHTML = '';
+    const opposingPlayerPiece = (selectedPiece + parseInt(clickedSquare.id))/2;
+    console.log(opposingPlayerPiece)
+  let toRemove = document.querySelectorAll(".square")
+  toRemove[opposingPlayerPiece].innerHTML='';
+    nextSquare = null;
+    //changePlayer();
+  } 
+} else {
+  nextSquare = clickedSquare;
 }
+    });
+  });
+
+} 
+
+
+
 function getWinner() {
   let redRemaining = false;
   let blackRemaining = false;
-  let blackMovesLeft = false;
-  let redMovesLeft = false;
 
-  board.forEach((piece, idx) => {
+  board.forEach((piece) => {
     if (piece === 'red') {
       redRemaining = true;
-      if (board[idx + 7] === '' || board[idx + 9] === '') {
-        redMovesLeft = true;
-      }
-    } else if (piece === 'black') {
+    }
+    if (piece === 'black') {
       blackRemaining = true;
-      if (board[idx - 7] === '' || board[idx - 9] === '') {
-        blackMovesLeft = true;
-      }
     }
   });
 
   if (!redRemaining) {
-    gameOver = true;
-    messageEl.style.fontSize = '40px';
-    messageEl.innerHTML = `Congratulations Black, You Win. Press PlayAgain if you would like to play again!`;
-    messageEl.style.color = 'black'
-    gameOver=true;
+    alert('Black Wins');
   } else if (!blackRemaining) {
-    gameOver = true;
-    messageEl.style.fontSize = '40px';
-    messageEl.innerHTML = `Congratulations Red, You Win. Press PlayAgain if you would like to play again!`;
-    messageEl.style.color = 'red'
-    gameOver=true;
-  } else if (!redMovesLeft) {
-    gameOver = true;
-    messageEl.style.fontSize = '40px';
-    messageEl.innerHTML = `Black Wins by Default. Press PlayAgain if you would like to play again!`;
-    messageEl.style.color = 'black'
-    gameOver=true;
-  } else if (!blackMovesLeft) {
-    gameOver = true;
-    messageEl.style.fontSize = '40px';
-    messageEl.innerHTML = `Red Wins by Default. Press PlayAgain if you would like to play again!`;
-    messageEl.style.color = 'red'
-    gameOver=true;
-  } else if (!redMovesLeft && !blackMovesLeft) {
-    gameOver = true;
-    messageEl.style.fontSize = '40px';
-    messageEl.innerHTML = `It's a Tie. Press PlayAgain if you would like to play again!`;
-    gameOver=true;
+    alert('Red Wins');
   }
-  renderControls();
 }
 
 function changePlayer() {
-    currentPlayer = currentPlayer === 'black' ? 'red' : 'black';
-    if (currentPlayer === 'red'){
-      messageEl.innerHTML = `${currentPlayer.toUpperCase()}'s Turn`;
-      messageEl.style.color = 'red'
-    }else if(currentPlayer === 'black'){
-    messageEl.innerHTML = `${currentPlayer.toUpperCase()}'s Turn`;
-    messageEl.style.color = 'black'
-    }
-  }
-
  
+};
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
